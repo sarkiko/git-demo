8289afea1e6e89194981817d0e4bb71fd925d025
sarkiko <sarkiko1st@gmail.com>
2020-12-03 20:00:12 +0200

    initial commit

---
+++ src/main/java/org/sarkiko/gitdemo/MainStart.java
@@ -0,0 +1,8 @@
+package org.sarkiko.gitdemo;
+
+public class MainStart {
+    public static void main(String[] args) {
+        String projectName = "git";
+        System.out.printf("Hello %s", projectName);
+    }
+}
