808ba973fe41361de73679637bdbc4d615d5326e
sarkiko <sarkiko1st@gmail.com>
2020-12-03 20:05:01 +0200

    changed project name

--- src/main/java/org/sarkiko/gitdemo/MainStart.java
+++ src/main/java/org/sarkiko/gitdemo/MainStart.java
@@ -2,7 +2,7 @@ package org.sarkiko.gitdemo;
 
 public class MainStart {
     public static void main(String[] args) {
-        String projectName = "git";
-        System.out.printf("Hello %s", projectName);
+        String projectName = "git demo";
+        System.out.printf("this is my %s project", projectName);
     }
 }
